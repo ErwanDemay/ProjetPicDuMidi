930a00c907d5580b799a76837d7b065315bbd91a
Antonylpz7 <lopezantony6400@gmail.com>
2024-10-17 16:43:32 +0200

    AntonyIndex

--- index.php
+++ index.php
@@ -1,25 +1,79 @@
+<?php
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Pic du midi application</title>
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="./main.css" rel="stylesheet"/>
-    <title> Pic du midi application</title>
+    
 </head>
 <body>
 <!--Création de colonne-->
- <main class="text-center border border-primary g-0  " >
+ <main class="text-center border border-primary   " >
 
-   <div class="row align-items-stretch g-0"> 
+   <div class="row align-items-stretch "> 
 
-   <div class="col-4 p_colonne_gauche g-0"> <!-- Colonne de gauche  -->
+   <div class="col-12 p_colonne_gauche "> <!-- Colonne principale  -->
    
    <div id="ImagePic">
-
-   <img src="./Image/PicduMidi" class="rounded-circle mx-auto d-block" width="50%" alt="image Pic">
-
+   <img src="./Image/PicduMidi.png" class="rounded-circle mx-auto d-block" width="20%" alt="image Pic">
    </div>
+   
+   <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark"   >
+  <div class="container-fluid">
+    <a class="navbar-brand col-3">Barre de navigation : </a>
+    
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    
+    <div class="col-3">
+    <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Option1</a>
+        </li>
+      </ul>
+      </div>
+
+      <div class="col-3">
+    <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Option2</a>
+        </li>
+      </ul>
+      </div>
+
+      <div class="col-3">
+    <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Option3</a>
+        </li>
+      </ul>
+      </div>
+
+      <div class="col-3">
+    <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Gerer Soirée</a>
+        </li>
+      </ul>
+      </div>
+
+
+
     </div>
+  </div>
+</nav>
+
+
+
+
+
+
+</main>
 </body>
 </html>
