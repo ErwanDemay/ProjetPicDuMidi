7df18e96497482c6ba423506c009df246ca6e341
Antonylpz7 <lopezantony6400@gmail.com>
2024-10-17 14:55:47 +0200

    InterfaceAntony

--- index.php
+++ index.php
@@ -3,12 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="./main.css" rel="stylesheet"/>
     <title> Pic du midi application</title>
 </head>
 <body>
-    <header>
-        
-    </header>
-    
+<!--Création de colonne-->
+ <main class="text-center border border-primary g-0  " >
+
+   <div class="row align-items-stretch g-0"> 
+
+   <div class="col-4 p_colonne_gauche g-0"> <!-- Colonne de gauche  -->
+   
+   <div id="ImagePic">
+
+   <img src="./Image/PicduMidi" class="rounded-circle mx-auto d-block" width="50%" alt="image Pic">
+
+   </div>
+    </div>
 </body>
 </html>
