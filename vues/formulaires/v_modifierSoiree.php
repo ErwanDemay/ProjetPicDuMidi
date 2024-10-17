ab718ebfeeb2ae9c67d1d6068957ea3ab0ccaba5
VictorLabeille <courssiovm@gmail.com>
2024-10-17 15:18:11 +0200

    gestion OK, formulaire en cours

---
+++ vues/formulaires/v_modifierSoiree.php
@@ -0,0 +1,34 @@
+<?php
+$id=$_GET['id'];
+
+
+?>
+
+<form action='/index.php?controleur=competences&action=competenceModifiee' method="POST">
+  <div>
+    <input type="hidden" name="id" id="id" value=' "<?php echo $id; ?>" '/>
+  </div>
+  <div>
+    <label for="nom">Nom :</label>
+    <input name="nom" id="nom" value="<?php echo $nom; ?>"/>
+  </div>
+  <div>
+    <label for="date">Date :</label>
+    <input name="date" id="date" value="<?php echo $nom; ?>"/>
+  </div>
+  <div>
+    <label for="lieu">Lieu :</label>
+    <input name="lieu" id="lieu" value="<?php echo $nom; ?>"/>
+  </div>
+  <div>
+    <label for="description">Description :</label>
+    <input name="description" id="description" value="<?php echo $nom; ?>"/>
+  </div>
+  <div>
+    <label for="nbPlaces">Nombre de places :</label>
+    <input name="nbPlaces" id="nbPlaces" value="<?php echo $nom; ?>"/>
+  </div>
+  <div>
+    <button>Modifier</button>
+  </div>
+</form>
