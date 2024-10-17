cd81c8d0cb000070a8d9c042054a6c9451e3cb47
madolipe <leonardoferigo02@gmail.com>
2024-10-17 15:55:13 +0200

    RequeteFini

--- soireeDAORequetes.php
+++ soireeDAORequetes.php
@@ -12,7 +12,7 @@ class SoireeDAO extends Base{
         $tableauSoirees = $resultatDeLaRequete->fetchAll();
         $lesObjSoirees = array();
         foreach($tableauSoirees as $uneLigneUneSoiree){
-            $uneSoiree = new Soiree($uneLigneUneSoiree ["id_soiree"],["nom_soiree"],["date_soiree"],["lieu"],["description"],["nbPlaces"]);
+            $uneSoiree = new soiree($uneLigneUneSoiree $uneSoiree["id_soiree"],$uneSoiree["nom_soiree"],$uneSoiree["date_soiree"],$uneSoiree["lieu"],$uneSoiree["description"],$uneSoiree["nbPlaces"]);
             $lesObjSoirees[] = $uneSoiree;
         }
         return $lesObjSoirees;
@@ -28,8 +28,20 @@ class SoireeDAO extends Base{
         return $resultatDeLaRequete;
     }
     
-    public function editSoiree($nom){
-        $resultatDeLaRequete=$this->exec(" WHERE nom=".$nom.";");
+    public function editSoiree($soiree){
+        $resultatDeLaRequete=$this->exec("UPDATE soirees SET `nom_soiree`, `date_soiree`,`lieu`,`description`,`nbPlaces`='".$soiree->getNom()."','".$soiree->getDate()."','".$soiree->getLieu()."','".$soiree->getDescription()."'),'".$soiree->getNbPlaces()."';");
         return $resultatDeLaRequete;
     }
+
+    public function getUneSoiree($id){
+        $resultatDeLaRequete=$this->query("SELECT * FROM soirees WHERE id='".$id."';");
+        $uneSoiree = $resultatDeLaRequete->fetch();
+        $unObjSoiree = new soiree($uneSoiree["id_soiree"],$uneSoiree["nom_soiree"],$uneSoiree["date_soiree"],$uneSoiree["lieu"],$uneSoiree["description"],$uneSoiree["nbPlaces"]);
+        return $unObjSoiree;
+    }
+
+
+
+
+
 }
