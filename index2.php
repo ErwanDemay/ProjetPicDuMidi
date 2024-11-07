52283e143dd23891ac81b71ab9c3d4d19d631a0c
VictorLabeille <courssiovm@gmail.com>
2024-11-07 16:36:11 +0100

    index2 pas fonctionnel (caca) et bouton avec logo sans image

---
+++ index2.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<script src="script.js"></script>
+<link href="CSS" rel="stylesheet"/>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Le Pic du Midi</title>
+    
+</head>
+<body>
+
+<div class="navbar">
+    <img src="./Image/PicduMidiLogo.webp" alt="Logo du Pic du Midi" class="logo">
+    
+    <div class="nav-buttons">
+        <a class="nav-button" href="./index2.php?controleur=soiree">Gérer soirée</a>
+        <button class="nav-button">Prochaine soirée</button>
+        <button class="nav-button">Retour à l'acceuil</button>
+        <button class="nav-button">À propos</button>
+    </div>
+</div>
+
+
+<?php
+            if (isset($_GET['controleur']))
+				$controleur=filter_var($_GET['controleur'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+			else
+				$controleur= "general";
+                    
+            switch ($controleur){
+                    case 'soiree' : 
+                            include("./controleurs/controleurSoirees.php"); 
+                            break;
+              }
+?>
+
+
+<div class="content">
+    <h1>LE PIC DU MIDI 2877M</h1>
+    <h2>Le Balcon des Pyrénées</h2>
+    <p>Venez vivre un moment hors du temps en contemplant la vue à couper le souffle qui s'offre à vous.</p>
+
+    <div class="button-container">
+        <button class="button">
+            <div class="button-text">
+                <span>Visite Journée Hiver</span>
+                <span class="button-price">Dès 34€</span>
+            </div>
+        </button>
+        <button class="button">
+            <div class="button-text">
+                <span>Bons cadeaux</span>
+                <span class="button-price">Dès 41€</span>
+            </div>
+        </button>
+        <button class="button">
+            <div class="button-text">
+                <span>Pass Ski Freeride</span>
+                <span class="button-price">Dès 48€</span>
+            </div>
+        </button>
+    </div>
+
+    <div class="footer">
+        OUVERT de 9h à 17h30 (dernière montée à 16h)
+    </div>
+</div>
+
+
+
+
+
+<script>
+
+    const images = [
+        'url("./Image/PicduMidi3.webp")',
+        'url("./Image/PicduMidi2.png")',
+        'url("./Image/PicduMidi.png")'
+    ];
+
+    let currentImageIndex = 0;
+
+    function changeBackgroundImage() {
+        document.body.style.backgroundImage = images[currentImageIndex];
+        currentImageIndex = (currentImageIndex + 1) % images.length;
+    }
+
+    changeBackgroundImage();
+    setInterval(changeBackgroundImage, 5000);
+
+</script>
+</body>
+</html>
--- vues/v_consultationSoirees.php
+++ vues/v_consultationSoirees.php
@@ -3,10 +3,12 @@
 	<?php
 		foreach ($lesSoirees as $Soiree) {
 	            echo $Soiree->getNom()." ";
-                    echo "<button><a href='index.php?controleur='soiree'&action='supprimerSoiree'&id=".$Soiree->getId()."'>Supprimer</a></button> ";
-                    echo "<button><a href='index.php?controleur=soiree&action=modifierSoiree&id=".$Soiree->getId()."'>Modifier</a></button>";
+                    //echo "<button><a href='index.php?controleur='soiree'&action='supprimerSoiree'&id=".$Soiree->getId()."'>Supprimer</a></button> ";
+                    echo "<button><a href='index.php?controleur='soiree'&action='supprimerSoiree'&id=".$Soiree->getId()."'><img src='../images/supprimer.png'></a></button>";
+                    echo "<button><a href='index.php?controleur=soiree&action=modifierSoiree&id=".$Soiree->getId()."'><img src='../images/modifier.png'></a></button>";
+                    //echo "<button><a href='index.php?controleur=soiree&action=modifierSoiree&id=".$Soiree->getId()."'>Modifier</a></button>";
                     //Pas d'envoi d'autre infos que l'id, on récupèrera tout dans le formulaire de modification
                     echo "<br><br>";
         }
 	?>
-</div>
+</div><img src="" alt="">
