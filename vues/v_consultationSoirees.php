8592b1cc640a7aa7fdddc7cff27ce3d0c17de991
VictorLabeille <courssiovm@gmail.com>
2024-11-07 16:38:55 +0100

    index2 catastrophe logo boutons ok mais gros

--- vues/v_consultationSoirees.php
+++ vues/v_consultationSoirees.php
@@ -4,8 +4,8 @@
 		foreach ($lesSoirees as $Soiree) {
 	            echo $Soiree->getNom()." ";
                     //echo "<button><a href='index.php?controleur='soiree'&action='supprimerSoiree'&id=".$Soiree->getId()."'>Supprimer</a></button> ";
-                    echo "<button><a href='index.php?controleur='soiree'&action='supprimerSoiree'&id=".$Soiree->getId()."'><img src='../images/supprimer.png'></a></button>";
-                    echo "<button><a href='index.php?controleur=soiree&action=modifierSoiree&id=".$Soiree->getId()."'><img src='../images/modifier.png'></a></button>";
+                    echo "<button><a href='index.php?controleur='soiree'&action='supprimerSoiree'&id=".$Soiree->getId()."'><img src='Images/supprimer.png'></a></button>";
+                    echo "<button><a href='index.php?controleur=soiree&action=modifierSoiree&id=".$Soiree->getId()."'><img src='Images/modifier.png'></a></button>";
                     //echo "<button><a href='index.php?controleur=soiree&action=modifierSoiree&id=".$Soiree->getId()."'>Modifier</a></button>";
                     //Pas d'envoi d'autre infos que l'id, on récupèrera tout dans le formulaire de modification
                     echo "<br><br>";
