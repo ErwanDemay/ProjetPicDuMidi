6d31323030949008549df779cf5933356e59df59
VictorLabeille <courssiovm@gmail.com>
2024-11-07 16:46:13 +0100

    bouton & logo ok

--- vues/v_consultationSoirees.php
+++ vues/v_consultationSoirees.php
@@ -4,8 +4,8 @@
 		foreach ($lesSoirees as $Soiree) {
 	            echo $Soiree->getNom()." ";
                     //echo "<button><a href='index.php?controleur='soiree'&action='supprimerSoiree'&id=".$Soiree->getId()."'>Supprimer</a></button> ";
-                    echo "<button><a href='index.php?controleur='soiree'&action='supprimerSoiree'&id=".$Soiree->getId()."'><img src='Images/supprimer.png' max-width='50px' max-height='50px'></a></button>";
-                    echo "<button><a href='index.php?controleur=soiree&action=modifierSoiree&id=".$Soiree->getId()."'><img src='Images/modifier.png'></a></button>";
+                    echo "<button><a href='index.php?controleur='soiree'&action='supprimerSoiree'&id=".$Soiree->getId()."'><img src='Images/supprimer.png' width='25px' height='25px'></a></button>";
+                    echo "<button><a href='index.php?controleur=soiree&action=modifierSoiree&id=".$Soiree->getId()."'><img src='Images/modifier.png' width='25px' height='25px'></a></button>";
                     //echo "<button><a href='index.php?controleur=soiree&action=modifierSoiree&id=".$Soiree->getId()."'>Modifier</a></button>";
                     //Pas d'envoi d'autre infos que l'id, on récupèrera tout dans le formulaire de modification
                     echo "<br><br>";
