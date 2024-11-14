e65016105b9ba9f76f38cf770cbb69882bad9f87
VictorLabeille <courssiovm@gmail.com>
2024-11-14 14:10:18 +0100

    Terminé, pas de bouton ajouter

--- vues/v_consultationSoirees.php
+++ vues/v_consultationSoirees.php
@@ -2,12 +2,18 @@
 <h2> Les soirées enregistrées sont : </h2>
 	<?php
 		foreach ($lesSoirees as $Soiree) {
-	            echo $Soiree->getNom()." ";
-                    //echo "<button><a href='index.php?controleur='soiree'&action='supprimerSoiree'&id=".$Soiree->getId()."'>Supprimer</a></button> ";
+                    //Affichage des informations de la soirées
+                    echo        "<p>Nom : ".$Soiree->getNom()."</p>".
+                                "<p>Date : ".$Soiree->getDate()."</p>".
+                                "<p>Lieu : ".$Soiree->getLieu()."</p>".
+                                "<p>Description : ".$Soiree->getDescription()."</p>".
+                                "<p>Nombre de places : ".$Soiree->getNbPlaces()."</p>";
+
                     echo "<button><a href='index.php?controleur=soiree&action=supprimerSoiree&id=".$Soiree->getId()."'><img src='Images/supprimer.png' width='25px' height='25px'></a></button>";
                     echo "<button><a href='index.php?controleur=soiree&action=modifierSoiree&id=".$Soiree->getId()."'><img src='Images/modifier.png' width='25px' height='25px'></a></button>";
-                    //echo "<button><a href='index.php?controleur=soiree&action=modifierSoiree&id=".$Soiree->getId()."'>Modifier</a></button>";
                     //Pas d'envoi d'autre infos que l'id, on récupèrera tout dans le formulaire de modification
+
+                    //Ligne et alinéas pour séparer les soirées
                     echo "<br><br> ------------------------------------ <br><br>";
         }
 	?>
