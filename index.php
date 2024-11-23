50609983ca068ff73665603cc98cf2aa5706bc76
VictorLabeille <courssiovm@gmail.com>
2024-11-23 20:51:30 +0100

    accueil fini (boutons)

--- index.php
+++ index.php
@@ -7,39 +7,57 @@
   <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
-
-<div id="ombre">
-<div class="diaporama">
-  <div class="diaporamaContenu">
-    <img class="diapo" src="../Images/PicduMidi1.png" alt="Image 1">
-    <img class="diapo" src="../Images/PicduMidi2.png" alt="Image 2">
-    <img class="diapo" src="../Images/PicduMidi3.png" alt="Image 3">
-  </div>
-</div>
+<div id='ombre'>
 
   <nav>
     <ul>
-      <li><a class="navBoutton" href="./index2.php">Accueil</a></li>
-      <li><a class="navBoutton" href="./index2.php?controleur=soiree&action=prochainesSoirees">Prochaines soirées</a></li>
+      <li><a class="navBoutton" href="./index.php">Accueil</a></li>
+      <li><a class="navBoutton" href="./index.php?controleur=soiree&action=prochainesSoirees">Prochaines soirées</a></li>
       <li class="sousMenu">
         <a class="navBoutton" href="#">Gestion</a>
         <ul class="sousMenuUl">
-          <li><a class="" href="./index2.php?controleur=soiree">Soirées</a></li>
-          <li><a class="" href="./index2.php?controleur=materiel">Matériels</a></li>
+          <li><a class="" href="./index.php?controleur=soiree">Soirées</a></li>
+          <li><a class="" href="./index.php?controleur=materiel">Matériels</a></li>
         </ul>
       </li>
     </ul>
   </nav>
 
-  <div id="contenuIndex">
-  <div id="txtAccueil">
-    <h1>LE PIC DU MIDI 2877M</h1>
-    <h2>Le Balcon des Pyrénées</h2>
-    <p class="pAccueil">Venez vivre un moment hors du temps en contemplant la vue à couper le souffle qui s'offre à vous. Bienvenue au Pic du Midi…</p>
-    <br>
-    <button id="buttonBilleterie">Billeterie</button>
-  </div>
-</div>
+
+<?php
+            if (isset($_GET['controleur']))
+				$controleur=filter_var($_GET['controleur'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+			else
+				$controleur= "general";
+                    
+            switch ($controleur){
+                    case 'general':
+                        echo    "<div class='diaporama'>".
+                                "<div class='diaporamaContenu'>".
+                                  "<img class='diapo' src=\"../Images/PicduMidi1.png\" alt='Image 1'>".
+                                  "<img class='diapo' src=\"../Images/PicduMidi2.png\" alt='Image 2'>".
+                                  "<img class='diapo' src=\"../Images/PicduMidi3.png\" alt='Image 3'>".
+                                "</div>".
+                              "</div>".
+                              "<div id='contenuIndex'>".
+                                "<div id='txtAccueil'>".
+                                  "<img src='./Images/PicduMidiLogo.png' alt='Logo du Pic du Midi'>".
+                                  "<h1>LE PIC DU MIDI 2877M</h1>".
+                                  "<h2>Le Balcon des Pyrénées</h2>".
+                                  "<p class='pAccueil'>Venez vivre un moment hors du temps en contemplant la vue à couper le souffle qui s'offre à vous. Bienvenue au Pic du Midi…</p>".
+                                  "<br>".
+                                  "<button id='buttonBilleterie'>Billeterie</button>".
+                                "</div>".
+                              "</div>";
+                        break;
+                    case 'soiree' : 
+                            include("./controleurs/controleurSoirees.php"); 
+                            break;
+                    case 'materiel' : 
+                            include("./controleurs/controleurMateriel.php"); 
+                            break;
+              }
+?>
 </div>
 
 </body>
--- indexBKP.php
+++ indexBKP.php
@@ -3,41 +3,65 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Pic du Mid</title>
-  <link rel="stylesheet" type="text/css" href="css/index.css">
+  <title>Pic du Midi</title>
+  <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
 
+<div id="ombre">
+<div class="diaporama">
+  <div class="diaporamaContenu">
+    <img class="diapo" src="../Images/PicduMidi1.png" alt="Image 1">
+    <img class="diapo" src="../Images/PicduMidi2.png" alt="Image 2">
+    <img class="diapo" src="../Images/PicduMidi3.png" alt="Image 3">
+  </div>
+</div>
+
   <nav>
-
-    <li class="liPrincipale">
-      <input type="checkbox" id="check">
-      <label for="check" class="checkbtn">
-        <!--<i class="fas fa-bars"></i> element for the hamburger icon-->
-        <p id="btnGestion">Gestion ⯆ </p>
-      </label>
-      <div class="navMobile">
-        <a class="navButton">Soirées</a>
-        <a class="navButton">Matériels</a>
-      </div>
-    </li>
-
-    <li class="liPrincipale"><a class="navButton" href="./index2.php?controleur=soiree">Gérer soirée</a></li>
-    <li class="liPrincipale"><a class="navButton" href="./index2.php?controleur=materiel">Gérer matériels</a></li>
-    <li class="liPrincipale"><a class="navButton" href="./index2.php?controleur=soiree&action=prochainesSoirees">Prochaines soirées</a></li>
-    <li class="liPrincipale"><a class="navButton" href="./index2.php">Accueil</a></li>
-  
+    <ul>
+      <li><a class="navBoutton" href="./index2.php">Accueil</a></li>
+      <li><a class="navBoutton" href="./index2.php?controleur=soiree&action=prochainesSoirees">Prochaines soirées</a></li>
+      <li class="sousMenu">
+        <a class="navBoutton" href="#">Gestion</a>
+        <ul class="sousMenuUl">
+          <li><a class="" href="./index2.php?controleur=soiree">Soirées</a></li>
+          <li><a class="" href="./index2.php?controleur=materiel">Matériels</a></li>
+        </ul>
+      </li>
+    </ul>
   </nav>
 
-
+  <div id="contenuIndex">
   <div id="txtAccueil">
     <h1>LE PIC DU MIDI 2877M</h1>
     <h2>Le Balcon des Pyrénées</h2>
-    <br>
     <p class="pAccueil">Venez vivre un moment hors du temps en contemplant la vue à couper le souffle qui s'offre à vous. Bienvenue au Pic du Midi…</p>
     <br>
-    <button class="buttonBilleterie">Billeterie</button>
+    <button id="buttonBilleterie">Billeterie</button>
   </div>
+</div>
+</div>
+
+
+<?php
+            if (isset($_GET['controleur']))
+				$controleur=filter_var($_GET['controleur'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+			else
+				$controleur= "general";
+                    
+            switch ($controleur){
+                    case 'general':
+                        
+                        break;
+                    case 'soiree' : 
+                            include("./controleurs/controleurSoirees.php"); 
+                            break;
+                    case 'materiel' : 
+                            include("./controleurs/controleurMateriel.php"); 
+                            break;
+              }
+?>
+
 
 </body>
 </html>
