2050dd3b5d1a2f888f0768b64198b5c0d93b78ef
VictorLabeille <courssiovm@gmail.com>
2024-11-23 15:53:39 +0100

    matériel dans tableau

--- vues/v_consultationMateriel.php
+++ vues/v_consultationMateriel.php
@@ -35,21 +35,26 @@
     <i class="bi bi-plus-circle"></i> Créer un matériel</a>
 
 <h2>Voici la liste des matériels enregistrées : </h2>
+<table>
+    <tr> <th>Nom</th> <th>État</th></tr>
 	<?php
 		foreach ($lesMateriels as $Materiel) {
             
                     //Affichage des informations de la soirées
-                    echo        "<p>Nom : ".$Materiel->getNom()."</p>".
-                                "<p>État : ".$Materiel->getEtat()."</p><br>";
+                    echo        "<tr>".
+                                "<td><p>Nom : ".$Materiel->getNom()."</p></td>".
+                                "<td><p>État : ".$Materiel->getEtat()."</p></td>";
 
                     echo "<button><a href='index2.php?controleur=materiel&action=supprimerMateriel&id=".$Materiel->getId()."'><img src='Images/supprimer.png' width='25px' height='25px'></a></button>";
                     echo "<button><a href='index2.php?controleur=materiel&action=modifierMateriel&id=".$Materiel->getId()."'><img src='Images/modifier.png' width='25px' height='25px'></a></button>";
                     //Pas d'envoi d'autre infos que l'id, on récupèrera tout dans le formulaire de modification
 
                     //Alinéas pour séparer les soirées
-                    echo "<br><br><br><br>";
+                    echo "</tr>";
         }
 	?>
+</table>
     <br><br><br><br><br><br>
+
 </div>
 </div>
