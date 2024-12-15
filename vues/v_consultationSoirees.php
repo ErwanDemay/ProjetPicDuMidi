759b8c843ce1fc1d59fb650763d46aa57d5f9b90
erwansio1 <erwan.demay64140@gmail.com>
2024-12-15 15:38:46 +0100

    tableau

--- vues/v_consultationSoirees.php
+++ vues/v_consultationSoirees.php
@@ -10,38 +10,38 @@
     <h2>Voici la liste des soirées enregistrées :</h2>
   </div>
 
-<h2>Voici la liste des soirées enregistrées : </h2>
-<table>
-    <tr> <th>Nom</th> <th>Date</th> <th>Lieu</th> <th>Description</th> <th>Nombre de places</th> <th>Nombre de places restantes</th> <th>Prix</th> <th>Heure de début</th> <th>Supprimer</th> <th>Modifier</th></tr>
-	<?php
-
-        if($lesSoirees == null){
-            echo "<h3>Aucune soirée à venir</h3>";
-        }else{
-
-            foreach ($lesSoirees as $Soiree) {
-
-                
-                        //Affichage des informations de la soirées
-                        echo        "<tr>".
-                                    "<td><p>".$Soiree->getNom()."</p></td>".
-                                    "<td><p>".$Soiree->getDate()."</p></td>".
-                                    "<td><p>".$Soiree->getLieu()."</p></td>".
-                                    "<td><p>".$Soiree->getDescription()."</p></td>".
-                                    "<td><p>".$Soiree->getNbPlaces()."</p></td>".
-                                    "<td><p>".$connexionBD->getNbPlacesRestantes($Soiree)."</p></td>".
-                                    "<td><p>".$Soiree->getPrix()."</p></td>".
-                                    "<td><p>".$Soiree->getHeureDebut()."</p></td>";
-
-                                    
-
-                        echo "<td><button><a href='index.php?controleur=soiree&action=supprimerSoiree&id=".$Soiree->getId()."'><img src='Images/supprimer.png' width='25px' height='25px'></a></button></td>";
-                        echo "<td><button><a href='index.php?controleur=soiree&action=modifierSoiree&id=".$Soiree->getId()."'><img src='Images/modifier.png' width='25px' height='25px'></a></button></td>";
-                        //Pas d'envoi d'autre infos que l'id, on récupèrera tout dans le formulaire de modification
-                        echo "</tr>";
-                        //Ligne et alinéas pour séparer les soirées
-                        //echo "<br><br> ------------------------------------ <br><br>";
-            }
+  <!-- Conteneur scrollable -->
+  <div class="tableau_scroll">
+    <table>
+      <thead>
+        <tr>
+          <th>Nom</th>
+          <th>Date</th>
+          <th>Lieu</th>
+          <th>Description</th>
+          <th>Nombre de places</th>
+          <th>Nombre de places restantes</th>
+          <th>Prix</th>
+          <th>Heure de début</th>
+          <th>Supprimer</th>
+          <th>Modifier</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        foreach ($lesSoirees as $Soiree) {
+          echo "<tr>" .
+            "<td><p>" . $Soiree->getNom() . "</p></td>" .
+            "<td><p>" . $Soiree->getDate() . "</p></td>" .
+            "<td><p>" . $Soiree->getLieu() . "</p></td>" .
+            "<td><p>" . $Soiree->getDescription() . "</p></td>" .
+            "<td><p>" . $Soiree->getNbPlaces() . "</p></td>" .
+            "<td><p>" . $connexionBD->getNbPlacesRestantes($Soiree) . "</p></td>" .
+            "<td><p>" . $Soiree->getPrix() . "</p></td>" .
+            "<td><p>" . $Soiree->getHeureDebut() . "</p></td>" .
+            "<td><button><a href='index.php?controleur=soiree&action=supprimerSoiree&id=" . $Soiree->getId() . "'><img src='Images/supprimer.png' width='25px' height='25px'></a></button></td>" .
+            "<td><button><a href='index.php?controleur=soiree&action=modifierSoiree&id=" . $Soiree->getId() . "'><img src='Images/modifier.png' width='25px' height='25px'></a></button></td>" .
+            "</tr>";
         }
         ?>
       </tbody>
